db487a51ab09dbe0308c1a37739dea4f45497c18
Oscar Miras <oscar.miras@gmail.com>
2023-05-18 12:22:57 +0200

    updated

--- src/Tarjeta.jsx
+++ src/Tarjeta.jsx
@@ -1,11 +1,10 @@
 /** Componente funcional que representa una tarjeta */
 /** Me gustaría poder configurar el titulo h2 de esta tarjetra y su descripción */
 /** Por convección, le llamamos a la variable que contiene todas las propiedades que nos pasan 'props' */
-function Tarjeta(props) {
-    console.log('Props que le pasamos: ', props);
+function Tarjeta({titulo, descripcion}) {
     return <div>
-        <h2>{props.titulo}</h2>
-        <p>{props.descripcion}</p>
+        <h2>{titulo}</h2>
+        <p>{descripcion}</p>
     </div>
 }
 
